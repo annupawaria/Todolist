7ed37d211751653cb2a0065141a9c215292302a2
annupawaria <134483932+annupawaria@users.noreply.github.com>
2024-02-22 15:12:07 +0530

    complete

--- my-react-app/src/component/Todolist.js
+++ my-react-app/src/component/Todolist.js
@@ -1,30 +1,52 @@
 import React, { useState } from "react";
 
 const Todolist = () => {
-    const[activity,setActivity]=useState("")
-    const[listData,setListData]=useState([])
-    function addActivity(){
-        setListData([...listData,activity])
-        console.log(listData)
-        setActivity("")
-    }
+  const [activity, setActivity] = useState();
+  const [listData, setListData] = useState([]);
+
+  function addActivity(i) {
+    setListData((listData) => {
+      const updatedList = [...listData, activity];
+      console.log(updatedList);
+      setActivity('')
+      return updatedList;
+    });
+  }
+  function Remove(i){
+const updatedListData = listData.filter((id,i)=>{
+ return i!=id
+})
+setListData(updatedListData)
+  }
+  function removeAll(){
+setListData([])
+  }
   return (
     <div className="container">
       <div className="header">
         TODO LIST
         <br />
-        <input type="text" value={activity} onChange={(e)=>setActivity(e.target.value)} placeholder="write your todo" />
+        <input
+          type="text"
+          placeholder="write your activity"
+          value={activity}
+          onChange={(e) => setActivity(e.target.value)}
+        />
         <button onClick={addActivity}>Add</button>
-        <p className="List-heading">here is your List:{")"}</p>
-        {listData!=[] && listData.map((data,i)=>{
-            return(
-                <>
-                <p key={i}><div className="listData">{data}
-                    </div></p>
-                </>
-            )
-
-        })}
+        <p className="List-heading">Here is your List:{")"}</p>
+        {listData!= [] &&
+          listData.map((data, i) => {
+            return (
+              <>
+                <p key={i}>
+                  <div>{data}</div>
+                  <div><button onClick={Remove}>Remove(-)</button></div>
+                </p>
+              </>
+            );
+          })}
+      {listData.length>1 &&  
+        <button onClick={removeAll}> Remove All</button>}
       </div>
     </div>
   );
